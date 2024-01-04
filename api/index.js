93e9c9e69f36e6390033c878c8d69a95dd2ed430
khoa.77pm <khoa.77pm@gmail.com>
2024-01-04 23:19:31 +0700

    init database and connect

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
 const app = express();
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log('Connected database'))
+    .catch((error) => console.log(error))
 
 app.listen(3000,()=>{console.log('App started in port 3000!!');})
