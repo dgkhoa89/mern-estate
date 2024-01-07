463fc543515b86f2329633d07c6460fd65612381
khoa.77pm <khoa.77pm@gmail.com>
2024-01-07 14:12:50 +0700

    API: create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-import { errorHandler } from '../utils/error.js';
+import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {    
     const {username, email, password} = req.body;
@@ -12,4 +13,29 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(errorHandler(400,'Error when signup. ' + error.message))
     }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email: email});
+        if(!validUser) {
+            return next(errorHandler(401,'User is not found: ' + email));
+        }
+        
+        const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!isValidPassword){
+            return next(errorHandler(401,'Invalid password'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.SECRETE_KEY);
+        const {password : pass, ...rest} = validUser._doc
+        res
+            .cookie('accessToken', token)
+            .status(200)
+            .json(rest)
+    } catch (error) {
+        return next(error)
+    }
+
+    
 }
