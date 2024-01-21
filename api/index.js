b10817d3283636214e4006d1b0af1eb2fff52b32
khoa.77pm <khoa.77pm@gmail.com>
2024-01-21 11:57:16 +0700

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,15 +3,16 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 import cors from 'cors'
 import cookieParser  from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO)
     .then(() => console.log('Connected database'))
@@ -21,7 +22,7 @@ app.listen(3000,()=>{console.log('App started in port 3000!!');})
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
-
+app.use('/api/listing',listingRouter)
 
 //error handling
 app.use((err, req, res, next)=>{
